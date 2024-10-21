497fbed6c362a362d9e074b78f70a30c9c8581f7
liquid-krn <alagoke@gmail.com>
2024-10-21 12:49:35 +0100

    updated server function

--- server.js
+++ server.js
@@ -2,34 +2,73 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
-const pg = require('pg');
+const knex = require('knex');
 const app = express();
 const saltRounds = 10;
 require('dotenv').config();
 
-const db = new pg.Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'moviedb',
-  password: process.env.DBPASSWORD,
-  port: 5432,
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: process.env.DBPASSWORD,
+    database: 'moviedb',
+  },
 });
-db.connect();
+
 
 app.use(bodyParser.json());
 app.use(cors());
 
 
+app.post('/signup', async (req, res) => {
+  const { email, password, repassword } = req.body;
+  const date = new Date();
+
+  if (password !== repassword) {
+    return res.status(400).json('Passwords do not match');
+  }
+
+  try {
+    const users = await db('users').where({ email }).select('*');
+
+    if (users.length > 0) {
+      return res.status(400).json('User already exists');
+    } else {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json('Error hashing password');
+        }
+
+        await db('users').insert({
+          email,
+          password: hash,
+          created_at: date,
+        });
+
+        console.log('User registered successfully');
+        return res.json('Signup success');
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('Internal server error');
+  }
+});
+
 app.post('/signin', async (req, res) => {
   const { email, password } = req.body;
 
   try {
+    const users = await db('users').where({ email }).select('*');
 
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+    if (users.length > 0) {
+      const user = users[0];
+      const isMatch = await bcrypt.compare(password, user.password);
 
-    if (result.rows.length > 0) {
-      const user = result.rows[0];
-        const isMatch = await bcrypt.compare(password, user.password);
       if (isMatch) {
         res.json('success');
         console.log('User logged in successfully');
@@ -45,39 +84,8 @@ app.post('/signin', async (req, res) => {
   }
 });
 
-app.post('/signup', async (req, res) => {
-  const { email, password, repassword } = req.body;
 
-  if (password !== repassword) {
-    return res.status(400).json('Passwords do not match');
-  }
 
-  try {
-    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [email]);
-
-    if (checkResult.rows.length > 0) {
-      return res.status(400).json('User already exists');
-    } else {
-      bcrypt.hash(password, saltRounds, async (err, hash) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).json('Error hashing password');
-        }
-
-        const result = await db.query(
-          "INSERT INTO users(email, password) VALUES ($1, $2)",
-          [email, hash]
-        );
-
-        console.log('User registered successfully:', result);
-        return res.json('Signup success');
-      });
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json('Internal server error');
-  }
-});
 
 app.listen(process.env.PORT || 3001, function () {
   console.log('Server working on port 3001');
